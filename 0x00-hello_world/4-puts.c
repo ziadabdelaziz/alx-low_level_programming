b2373bfb9fc9a2851db72abd1472b51564a22b9e
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-16 04:06:42 -0800

    print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-/*
+/**
  * This function print a string to the terminal with new line at the end
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
