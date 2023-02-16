e243ce86e8d42373ba700e09d8aff7e5855b4bc7
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-16 04:14:37 -0800

    print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - the function that runs the program
  * Describtion: This function print a string to the terminal 
- *		with new line at the end
+ * with new line at the end
  * Return: The program return 0 indicating successful execution
  */
 int main(void)
