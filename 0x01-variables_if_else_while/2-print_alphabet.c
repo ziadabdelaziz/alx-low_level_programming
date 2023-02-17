2488acad7b8d8a7bd9a14c8cd571fb4951c86ae1
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-17 05:01:13 -0800

    string iteration

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - program starting point
+* Description: print alphabet
+* Return: 0 indicates successfull execution
+*/
+int main(void)
+{
+    char *alpha;
+
+    alpha = "abcdefghijklmnopqrstuvwxyz";
+    while (*alpha != '\0')
+    {
+        putchar(*alpha);
+        alpha++;
+    }
+    putchar('\n');
+
+    return (0);
+}
