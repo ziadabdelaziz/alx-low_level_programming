63e84efece25789b17511a69bd1fd4625dbc4033
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-17 06:45:48 -0800

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - program starting point
+* Description: print hexa decimal digits
+* Return: 0 indicates successfull execution
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i < 'g'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
