8bcc0f1a9912341919cc7f01f03e258c29312010
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-17 04:45:21 -0800

    modulas

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,11 @@
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last = n%10;
+	last = n%10;
 	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last);
