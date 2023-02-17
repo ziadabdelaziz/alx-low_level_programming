f6a790aa9d01df8f2555f65225becb557e69e3e3
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-17 06:33:33 -0800

    print decimal digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - program starting point
+* Description: print alphabet lower and upper case
+* Return: 0 indicates successfull execution
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
