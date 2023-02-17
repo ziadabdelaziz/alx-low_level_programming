8e02ded4e3d04cc68aedd89a0176c71f7be3fb10
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-17 07:20:11 -0800

    more nested loops

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - program starting point
+* Description: print all combinations of three digit number
+* Return: 0 indicates successfull execution
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48 + (i - 48) + 1; j < 58; j++)
+		{
+			for(k = 48 + (j - 48) + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55)
+					continue;
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
