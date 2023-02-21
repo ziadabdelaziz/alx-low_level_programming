03d198255e017ea30a43852598f5b3bfaeca2ffd
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-21 13:58:06 -0800

    print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* main - program strting point
+* Describtion: print alphabet
+* Return: 0 - success
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+* print_alphabet - print alphabet
+* Describtion: prints alphabet letters
+* Return: void
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; a <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
