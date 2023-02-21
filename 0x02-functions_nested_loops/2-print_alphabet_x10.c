9446773dd739cea523cf24ca24cf4efa425d7134
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-21 14:19:24 -0800

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
 * print_alphabet_x10
@@ -11,6 +11,9 @@ void print_alphabet_x10(void)
 	int j;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
+		_putchar('\n');
+	}
 }
