13ae650a3aa23f535a6f0eccac123daad5eb7353
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-21 14:00:17 -0800

    print alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,16 +11,4 @@ int main(void)
 	return (0);
 }
 
-/**
-* print_alphabet - print alphabet
-* Describtion: prints alphabet letters
-* Return: void
-*/
-void print_alphabet(void)
-{
-	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
-}
