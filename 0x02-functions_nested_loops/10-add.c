a2a83b579770b6ad898b0b5ad603c40eb43a57cf
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-21 16:47:08 -0800

    add two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,12 @@
 
 /**
 * add - add function
+* @a: first integer number
+* @b: second integer number
 * Describtion: adds to integer numbers
 * Return: the sum
 */
 int add(int a, int b)
+{
 	return (a + b);
+}
