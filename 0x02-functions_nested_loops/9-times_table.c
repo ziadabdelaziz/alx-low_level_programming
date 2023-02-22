d043d9551e38e079fee207bd358716267460b62e
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-21 16:31:52 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+*
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+	int mult;
+
+	for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
+		{
+			mult = i * j;
+			if (mult < 9 && j != 0)
+				_putchar(' ');
+
+			if (mult > 9)
+			{
+				_putchar('0' + mult / 10);
+				_putchar('0' + mult % 10);
+			}
+			else
+				_putchar('0' + mult);
+
+
+			if (j == 9)
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+}
