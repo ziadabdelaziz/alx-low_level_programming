5d49f8d765352588b62a768e66bcdae249d81e9b
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-21 16:09:47 -0800

    every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,13 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 		for (j = 0; j < 60; j++)
-			{
-				_putchar('0' + (i / 10));
-				_putchar('0' + (i % 10));
-				_putchar(':');
-				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
-				_putchar('\n');
-			}
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			_putchar(':');
+			_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+			_putchar('\n');
+		}
 }
 
