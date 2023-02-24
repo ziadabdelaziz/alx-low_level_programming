b648467d9ba2688d78c5e0acf2e13ce69dce6564
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-23 17:02:47 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,18 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int temp;
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 48; i <= 62; i++)
 		{
+			temp = i;
 			if (i > 57)
 			{
 				_putchar('1');
-				_putchar(i - 10);
+				temp -= 10;
 			}
+			_putchar(temp);
 		}
 		_putchar('\n');
 	}
