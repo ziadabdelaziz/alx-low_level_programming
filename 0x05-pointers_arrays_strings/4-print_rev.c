bc5036dcf70dea2cb85631a9340ce59f128da4fa
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 14:45:28 -0800

    print string reversed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - print string reversed
+* @s: pointer to string beginning
+* Return: 0 indicates successfull execution
+*/
+void print_rev(char *s)
+{
+	char *c;
+
+	for (c = s; c != '\0'; c++)
+		c++;
+
+	while (c != s)
+	{
+		c--;
+		_putchar(*c);
+	}
+}
