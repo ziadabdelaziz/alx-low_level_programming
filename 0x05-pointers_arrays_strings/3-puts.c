3142da35aee75c0eeac3f3c663fb98e18a4a0d31
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 14:37:11 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _puts - print string
+* @str: pointer to string beginning
+* Return: 0 indicates successfull execution
+*/
+void _puts(char *str)
+{
+	int *c;
+
+	for (c = str; *c != '\0'; c++)
+		_putchar(*c);
+}
