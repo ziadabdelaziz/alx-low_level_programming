0d5bb44f704ea38f07485f479f313ba913941bac
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 14:28:06 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - return string length
+* @s: string
+* Return: 0 indicates successfull execution
+*/
+int _strlen(char *s)
+{
+	char *c;
+	int len;
+
+	for (c = s; *c != '\0'; c++)
+		len++;
+
+	return len;
+}
