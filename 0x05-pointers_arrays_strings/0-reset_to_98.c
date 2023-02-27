8ef303b562e9a927d253981613dfd2ae04089a35
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 14:10:16 -0800

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,8 @@
 /**
 * main - update memory value
 * @n: pointer to integer
+* Description: update the value stored in
+* the address given in memory
 * Return: 0 indicates successfull execution
 */
 void reset_to_98(int *n)
