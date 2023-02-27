5ef5ab343feae6db262973d42299e40d11fc5936
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 14:07:42 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* main - update memory value
+* Return: 0 indicates successfull execution
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+void _putchar(int c);
+
+void reset_to_98(int *n);
