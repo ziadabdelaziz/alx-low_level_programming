0151f1ced81451654c4e61917e23e554f46631a4
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 18:26:58 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strcpy - print every other character
+* @dest: buffer
+* @src: string
+* Return: 0 indicates successfull execution
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i - 1] == '\0'; i++)
+		*dest[i] = *src[i];
+
+	return (dest);
+}
