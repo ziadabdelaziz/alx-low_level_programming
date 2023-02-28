ce449d8d316a4fdac9e4fc7b8d988b173b3b846e
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-02-27 17:05:28 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - print every other character
+* @str: pointer to string beginning
+* Return: 0 indicates successfull execution
+*/
+void puts2(char *str)
+{
+	int ndx;
+
+	ndx = 0;
+	while (str[ndx] != '\0' && str[ndx + 1] != '\0')
+	{
+		_putchar(str[ndx]);
+		ndx +=2;
+	}
+	_putchar('\n');
+}
