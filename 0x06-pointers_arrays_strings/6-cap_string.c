8d7f38830433fae3e525a554fcb2281f047367f7
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-02 18:36:04 -0800

    capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 
 	for (i = 1; str[i] != '\0' && str[i + 1] != '\0'; i++)
 		if (str[i] == ' ' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			str[i] -= 32;
+			str[i + 1] -= 32;
 
 	return (str);
 }
