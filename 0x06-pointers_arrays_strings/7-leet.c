4f7a769cf7c092cd88a860cd8efaa483b894bc81
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-03 18:02:35 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,28 +13,37 @@ char *leet(char *str)
 
 	len = 0;
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		len++;
+	}
 
 	str1 = malloc(sizeof(char) * len);
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
+		{
 			str1[i] = 4;
-
+		}
 		else if (str[i] == 'e' || str[i] == 'E')
+		{
 			str1[i] = 3;
-
+		}
 		else if (str[i] == 'o' || str[i] == 'O')
+		{
 			str1[i] = 0;
-
+		}
 		else if (str[i] == 't' || str[i] == 'T')
+		{
 			str1[i] = 7;
-
+		}
 		else if (str[i] == 'l' || str[i] == 'L')
+		{
 			str1[i] = 1;
-
+		}
 		else
+		{
 			str1[i] = str[i];
+		}
 	}
 
 	str = str1;
