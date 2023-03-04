29eb15cd8bce8a4418e2fed8ade557d3f8946cd3
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-03 17:27:48 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* leet - encode string into 1337
+* @str: string
+* Return: 0 indicates successfull execution
+*/
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = 4;
+
+		if (str[i] == 'e' || str[i] == 'E')
+			str[i] = 3;
+
+		if (str[i] == 'o' || str[i] == 'O')
+			str[i] = 0;
+
+		if (str[i] == 't' || str[i] == 'T')
+			str[i] = 7;
+
+		if (str[i] == 'l' || str[i] == 'L')
+			str[i] = 1;
+	}
+
+	return (str);
+}
