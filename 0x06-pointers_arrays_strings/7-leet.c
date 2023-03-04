4e47de574167f52c45168ea8f24abc65f8ecb28c
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-03 17:48:23 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,36 @@
 char *leet(char *str)
 {
 	int i;
+	int len;
+	char *str1;
 
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	str1 = malloc(sizeof(char) * len);
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = 4;
+			str1[i] = 4;
 
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = 3;
+		else if (str[i] == 'e' || str[i] == 'E')
+			str1[i] = 3;
 
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = 0;
+		else if (str[i] == 'o' || str[i] == 'O')
+			str1[i] = 0;
 
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = 7;
+		else if (str[i] == 't' || str[i] == 'T')
+			str1[i] = 7;
 
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = 1;
+		else if (str[i] == 'l' || str[i] == 'L')
+			str1[i] = 1;
+
+		else
+			str1[i] = str[i];
 	}
+
+	str = str1;
 
 	return (str);
 }
