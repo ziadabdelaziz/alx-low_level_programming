8c97575a96c7199aff37307401765b6316799bc1
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-06 15:02:29 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr - search string
+* @s: string
+* @c: character
+* Return: pointer to first occurence
+*/
+char *_strchr(char *s, char c)
+{
+	char *target;
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			target = (s + i);
+			return target;
+		}
+	}
+
+	return (NULL);
+}
