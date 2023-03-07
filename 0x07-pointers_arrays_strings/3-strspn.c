6a7396387b549b3191b6cdf5e1d572582263d169
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-06 17:21:00 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strspn - length of prefix substring
+* @s: string
+* @accept: string
+* Return: matching characters in s from accept
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(accept + j) == *(s + i))
+			{
+				count++;
+				break;
+			}
+		}
+	}
+
+	return (count);
+}
