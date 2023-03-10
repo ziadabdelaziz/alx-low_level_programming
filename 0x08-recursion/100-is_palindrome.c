fcdb31d9376236b27549fc24dacb7651b263f55d
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-09 17:55:01 -0800

    is palindrom

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* check_palindrom - check palindrom
+* @s: string
+* @i: integer
+* @j: integer
+* Return: integer
+*/
+int check_palindrom(char *s, int i, int j)
+{
+	if (i > j)
+		return (1);
+
+	if (*(s + i) != *(s + j))
+		return (0);
+
+	return (is_prime(s, i + 1, j - 1));
+}
+
+/**
+* is_palindrome - is palindrom
+* @s: string
+* Return: integer
+*/
+int is_palindrome(char *s)
+{
+	int i;
+	int len;
+
+	len = 0;
+	for(i = 0; *(s + i) != '\0'; i++)
+		len++;
+
+	return (check_palindrom(s, 0, len - 1));
+}
