88ba8ff037fd172b7c78820ae555c4213c16c8b3
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-10 09:44:21 -0800

    init

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
 * @argv: list of argucments
 * Return: 0 - successfull execution
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
