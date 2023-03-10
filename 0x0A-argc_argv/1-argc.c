3d22362b75c030ae2c69161fba5201d1168fed92
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-10 09:52:32 -0800

    argc

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program starting point
+* @argc: number of arguments
+* @argv: list of argucments
+* Return: 0 - successfull execution
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d", argc);
+}
