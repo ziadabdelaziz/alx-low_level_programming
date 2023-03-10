719cc1057be22a81c7cb24554ff87a5140322d22
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-10 11:49:50 -0800

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - program starting point
+* @argc: number of arguments
+* @argv: list of argucments
+* Return: 0 - successfull execution
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (isdigit(argv[i][j]) || argv[i][j] == '-')
+				continue;
+
+			printf("Error");
+			return (0);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
