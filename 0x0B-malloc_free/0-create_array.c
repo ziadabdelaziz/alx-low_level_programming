0836b3b51563c9417d4205306106d4c254635bc6
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-14 08:19:04 -0700

    allocate array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* create_array - allocate and return array of chars
+* @size: number of elements
+* @c: character
+* Return: pointer to array of characters
+* if size = 0 -> NULL
+*/
+char *create_array(unsigned int size, char c)
+{
+    char *arr;
+    int i;
+
+    if (!size)
+        return (NULL);
+
+    arr = malloc(size);
+    for (i = 0;i < size; i++)
+        *(arr + i) = c;
+    *(arr + i) = '\0';
+
+    return (arr);
+}
