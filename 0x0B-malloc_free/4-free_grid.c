c9f4486dd1bcf2b4303e03f09afa7fd8c09b12a1
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-15 19:10:55 -0700

    free matrix

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_grid - free grid
+* @grid: two dimentional array
+* @height: integer
+* Return: None
+*/
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
