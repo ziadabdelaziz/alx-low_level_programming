0a83de9786db16b378032089aefe0379f749a584
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-17 11:00:39 -0700

    allocate array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _calloc - allocate memory
+* @nmemb: number of members
+* @size: number of bytes for each member
+* Return: allocated memory
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *memo;
+
+	memo = malloc (nmemb * size);
+	if (memo == NULL)
+		return (NULL);
+
+	return (memo)
+}
