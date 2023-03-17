93fe880845ef90c3c4d53be4950742fc256ac168
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-17 11:08:16 -0700

    allocate array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (memo == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmem * size; i++)
+	for (i = 0; i < nmemb * size; i++)
 		memo[i] = 0;
 
 	return (memo);
