bc0c5d55eb30f9d18b343187972b8ba641934436
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-20 19:34:46 -0700

    macro function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) abs(x)
-
-/**
-* abs - get absolute value
-* @x: integer
-* Return: absolute value of x
-*/
-int abs(int x)
-{
-	if (x < 0)
-		return (x * (-1));
-	else
-		return (x);
-}
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
