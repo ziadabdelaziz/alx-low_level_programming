b8566e1b1ce3354242761d9c2af3062f3c123029
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-20 18:25:20 -0700

    give a dog info

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* init_dog - initialize dog
+* @d: dog
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+* Return: None
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
