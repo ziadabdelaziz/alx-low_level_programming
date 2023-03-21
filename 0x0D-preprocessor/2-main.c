f3d69f4559bf1787ba8d39ef664e78e8199bd291
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-20 19:22:27 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print name of the file
+* Return: 0 in case of success
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
