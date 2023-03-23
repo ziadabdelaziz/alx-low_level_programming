f844d0283a637c28852316f1efb28e38be1ccfd9
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-22 17:33:15 -0700

    using passed function iteratively

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* array_iterator - check memory allocation success
+* @array: array of integers
+* @size: size of the array
+* @action: function
+* Return: None
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(*(array + sizeof(int) * i));
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,7 @@
 
 void _putchar(int c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
+
