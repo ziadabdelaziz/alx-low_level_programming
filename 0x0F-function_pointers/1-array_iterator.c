eee358d85ab276cdece550a2c8ab8ae4f99a65aa
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-22 17:34:38 -0700

    using passed function iteratively

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size / sizeof(int); i++)
 		action(*(array + sizeof(int) * i));
 }
