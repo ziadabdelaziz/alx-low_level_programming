540b1eb0a1c5426530799c670e690f4542f0aee6
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-23 19:44:54 -0700

    arch

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* get_op_func - select proper function
+* @s: operator
+* Return: function
+*/
+int (*get_op_func(char *s))(int, int)
+{
+        op_t ops[] = {
+                {"+", op_add},
+                {"-", op_sub},
+                {"*", op_mul},
+                {"/", op_div},
+                {"%", op_mod},
+                {NULL, NULL}
+        };
+        int i;
+
+        i = 0;
+        while (i < 5)
+        {
+                if (ops->op[i] == s[0])
+                        return (ops[i].f);
+                i++;
+        }
+
+        return (ops[5].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include <stdlib>
+#include "3-calc.h"
+
+/**
+* op_add - add two integers
+* @a: array of integers
+* @b: size of the array
+* Return: result of operation
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - subtract two integers
+* @a: array of integers
+* @b: size of the array
+* Return: result of operation
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiply two integers
+* @a: array of integers
+* @b: size of the array
+* Return: result of operation
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - division on two integers
+* @a: array of integers
+* @b: size of the array
+* Return: result of operation
+*/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+* op_mod - modulus of two integers
+* @a: array of integers
+* @b: size of the array
+* Return: result of operation
+*/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
+
