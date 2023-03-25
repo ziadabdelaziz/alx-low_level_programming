f62fc3f95130e36214daf65ddbcef5cc0fd4bbc7
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-24 17:59:29 -0700

    print variable number of string arguments

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,14 +10,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
+	char *temp;
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(valist, char *));
+		temp = va_arg(valist, char *);
+		if (temp == NULL)
+			printf("(nil)");
+		else
+			printf("%s", temp);
+
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
+
 	printf("\n");
 	va_end(valist);
 }
