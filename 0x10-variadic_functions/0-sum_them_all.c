1d5d92b2402042055119c82ba3ea37ff4dbbe21a
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-24 17:25:30 -0700

    variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,5 @@
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+
 /**
 * sum_them_all - return sum of arguments
 * @n: integer
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef VARIADIC_FUNC_H
 #define VARIADIC_FUNC_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 void _putchar(int c);
 int sum_them_all(const unsigned int n, ...);
 
