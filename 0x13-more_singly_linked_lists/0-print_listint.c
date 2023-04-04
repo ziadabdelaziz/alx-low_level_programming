234d0b5e01d3511ee02f07a8e38c09847a3bf53d
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-04-04 06:15:57 -0700

    traverse

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - print all elements of list
- * @h: singly linked list
- * Return: size of the list
- */
+* print_listint - print all elements of list
+* @h: singly linked list
+* Return: size of the list
+*/
 size_t print_listint(const listint_t *h)
 {
-    size_t cnt;
+	size_t cnt;
 
-    cnt = 0;
-    for (; h != NULL; h = h->next)
-    {
-        printf("%d\n", h->n);
-        cnt++;
-    }
+	cnt = 0;
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		cnt++;
+	}
 
-    return (cnt);
+	return (cnt);
 }
