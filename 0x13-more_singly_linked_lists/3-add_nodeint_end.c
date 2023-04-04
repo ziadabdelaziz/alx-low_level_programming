d3486706fcb540eae136dfc34a40e9b408e89bb8
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-04-04 07:01:23 -0700

    add to end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node to rear
+ * @head: pointer to singly linked list
+ * @n: integer
+ * Return: singly linked list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *cur;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	cur = *head;
+	while (cur->next)
+		cur = cur->next;
+
+	cur->next = new;
+
+	return (*head);
+}
