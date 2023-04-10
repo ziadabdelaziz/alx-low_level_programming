8573623c4fd2c5f33689a98d5dfb8f14dc33b33f
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-04-10 05:56:17 -0700

    binary representation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary representation
+ * @n: positive number
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		putchar((n & 1) + '0');
+	}
+	else
+	{
+		putchar('0');
+	}
+}
