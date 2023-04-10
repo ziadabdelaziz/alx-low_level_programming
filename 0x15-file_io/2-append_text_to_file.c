da2a36e1d36876bacf9f9aa7e9b831fca8127c9a
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-04-10 01:35:03 -0700

    append to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* append_text_to_file - append text to file
+* @filename: file
+* @text_content: string
+* Return: -1 if fails or 1 if success
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fd, rwr;
+	size_t len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	rwr = write(fd, text_content, len);
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
