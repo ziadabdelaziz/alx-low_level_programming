80fdc21cf5c18b8815216c818edea4d13ab58099
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-04-11 16:10:54 -0700

    flip bits fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int cnt;
 
-	for (cnt = 0; n | m; n <<= 1, m <<= 1)
+	for (cnt = 0; n || m; n <<= 1, m <<= 1)
 	{
 		if ((n & 1) != (m & 1))
 			cnt++;
