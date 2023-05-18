1ab35f8973a91e20817ead6de1a26bf11472d5ff
ziad <ziadabdelaziz1995@gmail.com>
2023-05-19 02:44:34 +0300

    [FIX]: adding comment

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - return nth node
+ * @head: doubly linked list
+ * @index: node's index
+ * Return: nth node or NULL if not found.
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
