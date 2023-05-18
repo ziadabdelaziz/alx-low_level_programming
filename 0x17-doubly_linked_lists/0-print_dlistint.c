b367ddcda5fea2be0681ea667c5f97963cc0302a
ziad <ziadabdelaziz1995@gmail.com>
2023-05-18 17:33:47 +0300

    [FIX]: bettycodestyle

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h)
-    {
-        printf("%i\n", h->n);
-        h = h->next;
-        len++;
-    }
+	while (h)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		len++;
+	}
 
-    return (len);
+	return (len);
 }
