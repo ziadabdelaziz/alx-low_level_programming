bcdc1aea4a4bbe45a9dcf242291430475a4deb1e
ziad <ziadabdelaziz1995@gmail.com>
2023-05-19 03:11:21 +0300

    [ADD]: add new node at specific index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - add node
+ * at a specific position
+ * @h: doubly linked list
+ * @idx: indes to insert in
+ * @n: integer value
+ * Return: new node or NULL if fails.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *cur = *h;
+	dlistint_t *prev = NULL;
+	dlistint_t *new = NULL;
+	unsigned int i = 0;
+
+	while (cur)
+	{
+		if (i == idx)
+			break;
+		i++;
+		prev = cur;
+		cur = cur->next;
+	}
+
+	if (i != idx)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = cur;
+	new->prev = prev;
+	cur->prev = new;
+	if (prev)
+		prev->next = new;
+
+	return (new);
+}
